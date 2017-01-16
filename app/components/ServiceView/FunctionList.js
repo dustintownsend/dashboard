5fbcfb1d68b49fb52cadf2d27863626a6b648664
Danny King <danny.newidea@gmail.com>
2017-01-16 14:47:03 -0800

    actions icons

--- app/components/ServiceView/FunctionList.js
+++ app/components/ServiceView/FunctionList.js
@@ -101,7 +101,7 @@ const FunctionList = (props) => {
 
           {functionEvents}
         </div>
-        <div className={styles.functionActions}>
+        {/*<div className={styles.functionActions}>
           <Button
             type='button'
             onClick={() => { props.runFunctionDeploy(service.id, functionName) }}
@@ -126,6 +126,36 @@ const FunctionList = (props) => {
           <Button onClick={() => shell.openItem(functionFilePath)}>
             Open in Editor
           </Button>
+        </div>*/}
+        <div className={styles.actions}>
+          <div className={styles.action} title="Show function's logs" onClick={() => { props.runLogs(service.id, functionName) }}>
+            <div className={styles.icon}>
+              <svg viewBox="0 0 100 100" style={{ width: '1em', height: '1em' }}>
+                <path className={styles.stroke} vectorEffect="non-scaling-stroke" d="M85,32.5H35 M35,50h50 M35,67.5h50 M25,85h60 c5.5,0,10-4.5,10-10V15H25v60c0,5.5-4.5,10-10,10S5,80.5,5,75V35h15" style={{stroke: 'rgb(85, 85, 85)', fill: 'none', strokeWidth: 1.5}} />
+              </svg>
+            </div>
+          </div>
+          <div className={styles.action} title="Open with editor" onClick={() => shell.openItem(functionFilePath)}>
+            <div className={styles.icon}>
+              <svg viewBox="0 0 100 100" style={{ width: '1em', height: '1em' }}>
+                <path className={styles.fill} vectorEffect="non-scaling-stroke" d="M41.6,82.8L34.3,80l24.1-62.8l7.4,2.8L41.6,82.8z M74.8,75.2l-5.6-5.6L88.8,50L69.2,30.4l5.6-5.6L100,50 L74.8,75.2z M25.2,75.2L0,50l25.2-25.2l5.6,5.6L11.2,50l19.6,19.6L25.2,75.2z" />
+              </svg>
+            </div>
+          </div>
+          <div className={styles.action} title="Invoke function" onClick={() => { props.showInvokeModal(service.id, functionName) }}>
+            <div className={styles.icon}>
+              <svg viewBox="0 0 100 100" style={{ width: '1em', height: '1em' }}>
+                <path className={styles.fill} vectorEffect="non-scaling-stroke" d="M59,50c0,5-4,9-9,9s-9-4-9-9s4-9,9-9S59,45,59,50z M14,41c-5,0-9,4-9,9s4,9,9,9s9-4,9-9S19,41,14,41z M86,41 c-5,0-9,4-9,9s4,9,9,9s9-4,9-9S91,41,86,41z" />
+              </svg>
+            </div>
+          </div>
+          <div className={styles.action} title="Deploy function" onClick={() => { props.runFunctionDeploy(service.id, functionName) }}>
+            <div className={styles.icon}>
+              <svg viewBox="0 0 100 100" style={{ width: '1em', height: '1em' }}>
+                <path className={styles.stroke} vectorEffect="non-scaling-stroke" d="M50,5h45c0,0,0,45,0,45 M50,50L95,5 M27.5,5H5v90 c0,0,90,0,90,0V72.5" style={{stroke: 'rgb(85, 85, 85)', fill: 'none', strokeWidth: 1.5}} />
+              </svg>
+            </div>
+          </div>
         </div>
       </div>
     )
